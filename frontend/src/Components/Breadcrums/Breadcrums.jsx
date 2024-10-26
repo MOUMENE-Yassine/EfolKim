33afc6b0aad49a35a266e751001a74d7a9fb9265
MOUMENE-Yassine <moumene.yassineus@gmail.com>
2024-10-26 22:26:58 +0100

    update BreadCrums file

--- frontend/src/Components/Breadcrums/Breadcrums.jsx
+++ frontend/src/Components/Breadcrums/Breadcrums.jsx
@@ -5,9 +5,11 @@ import arrow_icon from '../Assets/breadcrum_arrow.png'
 const Breadcrums = (props) => {
   const {product} = props;
   return (
+    <div>
+    <div className="breadcrums-space">.</div>
     <div className='breadcrums'>
-      <div className="breadcrums-space">.</div>
       HOME <img src={arrow_icon} alt="" /> SHOP <img src={arrow_icon} alt="" /> {product.category} <img src={arrow_icon} alt="" /> {product.name} 
+    </div>
     </div>
   )
 }
